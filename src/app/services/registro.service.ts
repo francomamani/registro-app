b45f5ea472522530e711ce052c6cda6b71730d29
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2022-08-10 16:05:50 -0400

    crud users

--- src/app/services/registro.service.ts
+++ src/app/services/registro.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistroService {
 
+  private base = environment.base;
   constructor(private http: HttpClient) { }
 
   getUsers() {
-    return this.http.get('http://localhost:3000/users');
+    return this.http.get(`${this.base}users`);
   }
 
   setUser(user: any) {
-    return this.http.post('http://localhost:3000/users', user);
+    return this.http.post(`${this.base}users`, user);
   }
 
   deleteUser(id: number) {
-    return this.http.delete(`http://localhost:3000/users/${id}`);
+    return this.http.delete(`${this.base}users/${id}`);
   }
 }
