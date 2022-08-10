638af4579e6fd7a15ca15767218a9b1bd2b93766
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2022-08-10 15:30:44 -0400

    validacion 2

--- src/app/registro-form/registro-form.component.ts
+++ src/app/registro-form/registro-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {RegistroService} from "../services/registro.service";
 import {Router} from "@angular/router";
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-registro-form',
@@ -19,13 +20,20 @@ export class RegistroFormComponent implements OnInit {
     this.formGroup = this.fb.group({
       nombres: [''],
       apellidos: [''],
-      edad: [null],
+      edad: [0],
       fecha_nacimiento: [''],
       fecha_inscripcion: [''],
       costo: [null],
     });
   }
 
+  public setEdad() {
+    const fecha_nacimiento = this.formGroup!!.get('fecha_nacimiento')!!.value;
+    const edad = DateTime.fromFormat(fecha_nacimiento, 'dd.MM.yyyy')
+      .diffNow('years')
+      .years;
+    this.formGroup!!.get('edad')!!.setValue(Math.abs(edad));
+  }
   public saveUser() {
     if(this.formGroup !== undefined) {
       if (this.formGroup.valid) {
